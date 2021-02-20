fe991d549f88584b7fc6a62fa2f88a0d9cf6fa31
Mark Kravinskiy <mark@kravinskiy.com>
2021-02-20 21:00:06 +0100

    Add models to main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "github.com/Kravinskiy/go-utilities/Functions"
 	_ "github.com/Kravinskiy/go-utilities/Types"
+	_ "github.com/Kravinskiy/go-utilities/Models"
 )
 
 func main() {
