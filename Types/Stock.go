aa6b1fad27341ebadb846d8ec0b9164828ae5fad
Mark Kravinskiy <mark@kravinskiy.com>
2021-02-20 16:23:59 +0100

    Add Types/Stock

---
+++ Types/Stock.go
@@ -0,0 +1,11 @@
+package Stock
+
+type Stock struct {
+	Currency string
+	Description string
+	DisplaySymbol string
+	Figi string
+	Mic string
+	Symbol string
+	Type string
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "github.com/Kravinskiy/go-utilities/Functions"
+	_ "github.com/Kravinskiy/go-utilities/Types"
 )
 
 func main() {
