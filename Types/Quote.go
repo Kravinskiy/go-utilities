13aa27d892fda69ed4de904a426b6eb839899241
Mark Kravinskiy <mark@kravinskiy.com>
2021-02-21 20:20:28 +0100

    Change Quote types to float32

--- Types/Quote.go
+++ Types/Quote.go
@@ -1,9 +1,9 @@
 package types
 
 type Quote struct {
-	C string // Current price
-	H string // High price of the day
-	L string // Low price of the day
-	O string // Open price of the day
-	Pc string // Previous close price
+	C float32 // Current price
+	H float32 // High price of the day
+	L float32 // Low price of the day
+	O float32 // Open price of the day
+	Pc float32 // Previous close price
 }
