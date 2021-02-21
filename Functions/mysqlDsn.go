cd462d1378e5e3d4bba4bede4f3a9930499ec53b
Mark Kravinskiy <mark@kravinskiy.com>
2021-02-21 04:25:35 +0100

    Add driver default values

--- Functions/mysqlDsn.go
+++ Functions/mysqlDsn.go
@@ -5,5 +5,5 @@ import (
 )
 
 func GetMysqlDsn(username string, password string, hostname string, port string, dbname string) string {  
-    return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", username, password, hostname, port, dbname)
+    return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, hostname, port, dbname)
 }
