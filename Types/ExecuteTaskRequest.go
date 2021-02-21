1e285d21e1887b2e8e4e4500586274dfe7e04d19
Mark Kravinskiy <mark@kravinskiy.com>
2021-02-21 20:17:53 +0100

    Change type to integer

--- Types/ExecuteTaskRequest.go
+++ Types/ExecuteTaskRequest.go
@@ -4,5 +4,5 @@ type ExecuteTaskRequest struct {
 	Type string `json:"type"`
 	Currency  string `json:"currency"`
 	Ticker_Symbol string `json:"ticker_symbol"`
-	Ticker_Symbol_Id string `json:"integer"`
+	Ticker_Symbol_Id int `json:"ticker_symbol_id"`
 }
